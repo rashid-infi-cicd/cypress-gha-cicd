c3c98aebbfd27ba8250d6e85889e95d854a391c5
rashid-infi-cicd <rashid.6689@gmail.com>
2024-08-22 11:32:23 +0500

    Mocha report integration

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,32 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter: 'cypress-mochawesome-reporter',
+    experimentalRunAllSpecs: true,
+    reporterOptions: {
+      reportTitle: 'Feather Cypress Mocha Report',
+      //reportPageTitle: 'Feather Cypress Mocha Report',
+      reportDir: 'cypress/reports/mocha',
+      overwrite: false,
+      html: true,
+      json: false,
+      //For single spec files, use the base name of the spec file
+      //reportFilename: 'report-[name]-[datetime]',
+      //For all specs, use "Combined Report"
+      reportDir: 'cypress/reports/mocha',
+      reportFilename: 'mochawesome',
+      screenshotOnRunFailure: true,
+      charts: true,
+      inlineAssets: true,
+      saveAllAttempts: true,
+      //saveOnlyFailures: false,
+    },
+    video: true,
+    videoCompression: true,
+    videoCompression: 15,
+    video: true,
     video: true,
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
